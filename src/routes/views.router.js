0b80e92a9b05edb514b88aea2f43d4772db31b69
Kevin Giorgi <kevingiorgi777@gmail.com>
2023-09-07 14:23:52 -0300

    2da pre entrega

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,17 +1,27 @@
-import {Router} from "express";
-import{ productManager } from "../managers/products/ProductManagerMongo.js"; 
-import {__dirname} from '../utils.js'
+import {
+    Router
+} from "express";
+import {
+    productManager
+} from "../managers/products/ProductManagerMongo.js";
+import {
+    __dirname
+} from '../utils.js'
 
 const router = Router()
 
 
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) => {
     try {
         const products = await productManager.getProducts()
         console.log(products);
-        res.render('home',{products: products})
+        res.render('home', {
+            products: products
+        })
     } catch (error) {
-        throw res.status(500).json({ error }) 
+        throw res.status(500).json({
+            error
+        })
     }
 })
 
@@ -29,25 +39,55 @@ router.get('/products', async(req,res)=>{
     const {limit=10,page=1,sort,...query} = req.query
     try {
         console.log(req.query);
+
         const products = await productManager.paginateRender(limit,page,sort,query)
-        console.log(products);
-        //res.status(200).json({message:'Products', products})
-        res.render('products',{products: products})
+        const info = products.info
+        const payload = info.payload
+
+        res.render('products',{info, payload})
     } catch (error) {
         throw res.status(500).json({error})
     }
 })
 
+/* router.get('/products', async (req, res) => {
+    try {
+        const allprod = await fetch('http://localhost:8080/api/views/products')
+        const getProd = await allprod.json()
+        const products = getProd.payload
+        const productsMap = products.map(e => ({
+            _id: e._id,
+            title: e.title,
+            description: e.description,
+            code: e.code,
+            price: e.price,
+            stock: e.stock,
+            category: e.category
+        }))
+        res.render("products", {
+            productsMap
+        })
+    } catch (error) {
+        error
+    }
+}); */
 
 
-router.get('/realtimeproducts', async (req,res)=>{
+
+
+
+router.get('/realtimeproducts', async (req, res) => {
     try {
         const products = await productManager.getProducts()
-        res.render('realTimeProducts',{products: products})
+        res.render('realTimeProducts', {
+            products: products
+        })
     } catch (error) {
-        throw res.status(500).json({ error }) 
+        throw res.status(500).json({
+            error
+        })
     }
-    
+
 })
 
 
